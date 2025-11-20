b44410081e31e2f046223a3d2e7af74cec417878
SRIKANTH CHAKATI <145051600+chsrkh@users.noreply.github.com>
2025-11-20 10:49:02 +0530

    Add files via upload

---
+++ Array_implementation_C.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+  
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  // Declaring array of size n
+
+    // Reading elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    printf("The elements in the array are:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
